0e6c17eca0cbb64f207e904a87b00623302b2c75
Prachi <raj.prachi290@gmail>
2023-06-16 18:14:10 +0530

    Feat:added Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar.js';
+import { Link,BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import Home from './Pages/Home';
 function App() {
   return (
-    <div className="App">
-      testing ci=false
+    <div className="App">  
+       <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="./Pages/Home" element={<Home/>} />
+        {/* <Route path="/products" element={<Products/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/admin" element={<Admin/>} /> */}
+      </Routes>
+      
+    </Router>
     </div>
   );
 }
---
+++ src/Pages/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const Home=()=>{
+    return(
+        <h>This is home</h>
+    )
+};
+export default Home;
